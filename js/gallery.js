29a12641844166b31e0c21efb473eb94192eb87a
kangurtboy <kangurt.boy@gmail.com>
2020-05-14 16:00:53 +0500

    просмотр фоторафии с помощю клавиатуры

--- js/gallery.js
+++ js/gallery.js
@@ -1,6 +1,8 @@
 (function () {
 	window.gallery = document.querySelector(".big-picture");
 	window.CurrentPhotoIndex = 0;
+	var focusedImg;
+	var galleryClose = window.gallery.querySelector(".big-picture__cancel");
 	function renderGallery(img) {
 		//отрисовка галлерея
 		window.gallery.classList.remove("hidden");
@@ -9,7 +11,6 @@
 		  .querySelector("img");
 		var galleryDescription = window.gallery.querySelector(".social__caption");
 		var galleryLike = window.gallery.querySelector(".likes-count");
-		var galleryClose = window.gallery.querySelector(".big-picture__cancel");
 		var allFotos = document.querySelectorAll(".picture__img");
 		for (var i = 0; i < window.data.pictures.length; i++) {
 		  if (img === allFotos[i].src) {
@@ -21,18 +22,29 @@
 		galleryLike.textContent = window.data.pictures[CurrentPhotoIndex].likes;
 		galleryDescription.textContent = window.data.pictures[CurrentPhotoIndex].description;
 		window.renderComments(window.data.pictures[CurrentPhotoIndex].comments);
-		galleryClose.addEventListener("click", function() {
-			gallery.classList.add("hidden");
-			window.resetComment();
-		});
 	};
 	var onClickPhotoItem = function(e) {
 		//открытие фото в галлереи
-		  if (e.target.className === "picture__img") {
-			  e.preventDefault();
-			renderGallery(e.target.src);
-			
-		}
+		e.preventDefault();
+		if (e.target.className === "picture__img") {
+			renderGallery(e.target.src);	
+		};
 	};
+	var onGalleryClose = function (e) {
+		gallery.classList.add("hidden");
+		window.resetComment();
+	};
+	document.addEventListener('keydown', function (e) {
+		var focusElement = document.activeElement;
+		 focusedImg = focusElement.querySelector('img');
+		
+	})
+	window.utils.keyEvent(27, onGalleryClose);
+	window.utils.keyEvent(13, function () {
+		if (focusedImg) {
+			renderGallery(focusedImg.src);
+		}
+	})
+	galleryClose.addEventListener("click", onGalleryClose);
 	document.body.addEventListener("click", onClickPhotoItem);
 })()
