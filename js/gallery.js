7fb02105031d6020b8b0353ba6339f60a83074d7
kangurtboy <kangurt.boy@gmail.com>
2020-05-14 16:30:40 +0500

    исправил ошибки

--- js/gallery.js
+++ js/gallery.js
@@ -3,19 +3,20 @@
 	window.CurrentPhotoIndex = 0;
 	var focusedImg;
 	var galleryClose = window.gallery.querySelector(".big-picture__cancel");
+	var commentInputElement = window.gallery.querySelector('.social__footer-text');
 	function renderGallery(img) {
 		//отрисовка галлерея
 		window.gallery.classList.remove("hidden");
 		var galleryImg = window.gallery
-		  .querySelector(".big-picture__img")
-		  .querySelector("img");
+			.querySelector(".big-picture__img")
+			.querySelector("img");
 		var galleryDescription = window.gallery.querySelector(".social__caption");
 		var galleryLike = window.gallery.querySelector(".likes-count");
 		var allFotos = document.querySelectorAll(".picture__img");
 		for (var i = 0; i < window.data.pictures.length; i++) {
-		  if (img === allFotos[i].src) {
-			CurrentPhotoIndex = i;
-		  }
+			if (img === allFotos[i].src) {
+				CurrentPhotoIndex = i;
+			}
 		};
 		window.currentComments = window.data.pictures[CurrentPhotoIndex].comments;
 		galleryImg.src = img;
@@ -23,28 +24,46 @@
 		galleryDescription.textContent = window.data.pictures[CurrentPhotoIndex].description;
 		window.renderComments(window.data.pictures[CurrentPhotoIndex].comments);
 	};
-	var onClickPhotoItem = function(e) {
+	var onClickPhotoItem = function (e) {
 		//открытие фото в галлереи
 		e.preventDefault();
 		if (e.target.className === "picture__img") {
-			renderGallery(e.target.src);	
+			renderGallery(e.target.src);
 		};
 	};
 	var onGalleryClose = function (e) {
+		//закрытие галлереи
 		gallery.classList.add("hidden");
 		window.resetComment();
 	};
-	document.addEventListener('keydown', function (e) {
+	var onFocusImg = function (e) {
+		//получение фокуса изображении при навигации клавиша tab
 		var focusElement = document.activeElement;
-		 focusedImg = focusElement.querySelector('img');
-		
-	})
-	window.utils.keyEvent(27, onGalleryClose);
+		focusedImg = focusElement.querySelector('img');
+	}
+	var onFocusInput = function (e) {
+		//при получение фокуса поля ввода комментарии
+		document.removeEventListener('keydown', onEsc);
+	};
+	var onBlurInput = function (e) {
+		//при потерии фокуса поля ввода комментарии
+		document.addEventListener('keydown', onEsc);
+	};
+	var onEsc = function (e) {
+		//при нажании клавиша esc
+		if (e.keyCode === 27) {
+			onGalleryClose();
+		}
+	}
+	document.addEventListener('keydown', onFocusImg);
+	commentInputElement.addEventListener('focus', onFocusInput);
+	commentInputElement.addEventListener('blur', onBlurInput);
+	document.addEventListener('keydown', onEsc);
 	window.utils.keyEvent(13, function () {
 		if (focusedImg) {
 			renderGallery(focusedImg.src);
 		}
-	})
+	});
 	galleryClose.addEventListener("click", onGalleryClose);
 	document.body.addEventListener("click", onClickPhotoItem);
-})()
+})();
